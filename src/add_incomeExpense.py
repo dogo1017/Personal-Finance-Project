10deabeb65f947a906dba09c50213176b6f1c712
IsCowdell <isabella.cowdell@ucas-edu.net>
2026-03-25 09:51:45 -0600

    Finished most of the menu for income/expense

---
+++ src/add_incomeExpense.py
@@ -0,0 +1,6 @@
+#asking user if they want to add income or expense or view total
+# or 3 return to menu 
+#If user inputs an 1 then take them to 
+# would they like to add 1.income or 2.expese or 3. Return menu
+#       If one then add income taking in date,amount and source 
+        #after add it to the csv called ......
