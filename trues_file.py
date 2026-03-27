359102ebf89a1d5f9e6eb26f15272eee528aca99
FixedFusion2 <true.eggleston@ucas-edu.net>
2026-03-27 09:41:31 -0600

    Pseudocode

--- trues_file.py
+++ trues_file.py
@@ -1,11 +1,45 @@
 # TE 2nd Personal Financial Calculator (True's Part)
 #Import Libraries
-#To-do list for Pseudocode
 
-    #Savings Goal Function
 
-    #Budget Function
-        #Show side by side expenses and purchases.
-        #Have user enter expenses and purchases.
-        #Compare Expenses to Purchases
-        #Set Budget Limits (pie chart)
+#Savings Goal Function
+    #While True
+        #Print 1. Set new savings goal
+        #Print 2. View savings goal
+        #Print 3. Return to menu
+        #savings_choice is set to an input asking the user to choose 1-3
+        #If savings_choice is set to 1
+            #savings_goal is set to a user float input asking them to enter a savings goal
+            #monthly is set to a user integer input asking them how much money they want to save per month
+            #length = savings_goal/monthly
+            #Print It will take you {length} months to reach your savings goal.
+                #save savings_goal, monthly, and length into CSV file
+        #Also If savings_goal is set to 2
+            #If savings_goal == None
+                #Print You need to make a savings goal first
+            #Else:
+                #Open savings_goal from CSV file
+        #Also If savings_goal is set to 3
+                #Run menu
+
+
+
+
+#Budget Function
+    #While True
+        #Print 1. Set Budget Limits & Set Expenses
+        #Print 2. Compare Expenses to Purchases
+        #Print 3. Return to Menu
+        #budget is set to a user input asking to choose 1-3
+        #IF budget is set to 1
+            #budget_limit is set to a user input asking them to enter their budget limit
+            #expenses is set to an input asking the user to enter their expenses
+            #save budget_limit and expeneses in CSV file
+        #Also If budget is set to 2
+            #IF budget_limit or expenses == None
+                #Print you need to set your budget limits and expenses
+            #Else
+                #Print expeneses and budget_limits
+        #Also If
+                #Run menu
+
