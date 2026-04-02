6791fc1f4743fdee1d2680b0234df0bdbdee7e1a
FixedFusion2 <true.eggleston@ucas-edu.net>
2026-04-02 09:48:36 -0600

    True's File

--- trues_file.py
+++ trues_file.py
@@ -3,26 +3,47 @@
 
 
 #Savings Goal Function
+def savings_goal():
     #While True
+    while True:
         #Print 1. Set new savings goal
+        print("[1] Set New Savings Goal")
         #Print 2. View savings goal
+        print("[2] View Savings Goal")
         #Print 3. Return to menu
+        print("[3] Return to menu")
         #savings_choice is set to an input asking the user to choose 1-3
+        savings_choice = input("Choose 1-3: ")
         #If savings_choice is set to 1
+        if savings_choice == "1":
             #savings_goal is set to a user float input asking them to enter a savings goal
+            savings_goal = float(input("Enter a savings goal: "))
             #monthly is set to a user integer input asking them how much money they want to save per month
+            monthly = int(input("How much money do you want to save per month: "))
             #length = savings_goal/monthly
+            length = savings_goal/monthly
             #Print It will take you {length} months to reach your savings goal.
+            content = print(f"\nYour savings goal is {savings_goal}.\nYou want to save {monthly} per month.\nIt will take you {length} months to reach your savings goal.\n")
+            print(content)
                 #save savings_goal, monthly, and length into CSV file
+            with open("docs\\data.csv", "a") as file:
+                file.write(content)
         #Also If savings_goal is set to 2
+        elif savings_goal == "2":
             #If savings_goal == None
+            if savings_goal == None:
                 #Print You need to make a savings goal first
+                print("You need to make a savings goal first.")
             #Else:
+            else:
                 #Open savings_goal from CSV file
+                print("File opened (add this later).")
         #Also If savings_goal is set to 3
+        elif savings_goal == "3":
                 #Run menu
+            print("Running Menu, add this later.")
 
-
+savings_goal()
 
 
 #Budget Function
